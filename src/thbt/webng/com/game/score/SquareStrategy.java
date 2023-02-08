f834e5c7772de6e4ab8b63dbefbb85a3a45a74f0
hoaftq <hoaftq@gmail.com>
2023-02-08 16:27:52 +0800

    Refactored score strategies

--- src/thbt/webng/com/game/score/SquareStrategy.java
+++ src/thbt/webng/com/game/score/SquareStrategy.java
@@ -1,93 +1,47 @@
 package thbt.webng.com.game.score;
 
 import java.awt.Color;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import thbt.webng.com.game.Position;
 import thbt.webng.com.game.Square;
 
-public class SquareStrategy extends ScoreStrategy {
+class SquareStrategy extends ScoreStrategy {
 
-	public SquareStrategy(Square[][] squareArray) {
-		super(squareArray);
+	public SquareStrategy(Square[][] squares) {
+		super(squares);
 	}
 
 	@Override
-	public List<Square> getCompleteArea(Position pos) {
-		List<Square> listCompleteSquare = new ArrayList<Square>();
+	public List<Square> getCompletedArea(Position pos) {
+		var completedSquares = new HashSet<Position>();
+		completedSquares.addAll(
+				getCompletedPositions(pos, List.of(Direction.TO_TOP, Direction.TO_RIGHT, Direction.TO_TOP_RIGHT)));
+		completedSquares.addAll(getCompletedPositions(pos,
+				List.of(Direction.TO_BOTTOM, Direction.TO_RIGHT, Direction.TO_BOTTOM_RIGHT)));
+		completedSquares.addAll(
+				getCompletedPositions(pos, List.of(Direction.TO_BOTTOM, Direction.TO_LEFT, Direction.TO_BOTTOM_LEFT)));
+		completedSquares.addAll(
+				getCompletedPositions(pos, List.of(Direction.TO_TOP, Direction.TO_LEFT, Direction.TO_TOP_LEFT)));
 
-		int row = squareArray.length;
-		int col = squareArray[0].length;
-		Color color = squareArray[pos.x][pos.y].getBall().getColor();
-		boolean b1 = false;
-		boolean b2 = false;
-		boolean b3 = false;
-
-		if (pos.x > 0 && pos.y > 0) {
-			if (squareArray[pos.x][pos.y - 1].isEnableDestroy(color)
-					&& squareArray[pos.x - 1][pos.y - 1].isEnableDestroy(color)
-					&& squareArray[pos.x - 1][pos.y].isEnableDestroy(color)
-
-			) {
-				listCompleteSquare.add(squareArray[pos.x][pos.y - 1]);
-				listCompleteSquare.add(squareArray[pos.x - 1][pos.y - 1]);
-				listCompleteSquare.add(squareArray[pos.x - 1][pos.y]);
-				b1 = true;
-			}
+		if (completedSquares.size() > 0) {
+			completedSquares.add(pos);
+			return completedSquares.stream().map(p -> squares[p.x][p.y]).toList();
 		}
 
-		if (pos.x > 0 && pos.y < row - 1) {
-			if (squareArray[pos.x - 1][pos.y].isEnableDestroy(color)
-					&& squareArray[pos.x - 1][pos.y + 1].isEnableDestroy(color)
-					&& squareArray[pos.x][pos.y + 1].isEnableDestroy(color)
+		return List.of();
+	}
 
-			) {
-				if (!b1) {
-					listCompleteSquare.add(squareArray[pos.x - 1][pos.y]);
-				}
-				listCompleteSquare.add(squareArray[pos.x - 1][pos.y + 1]);
-				listCompleteSquare.add(squareArray[pos.x][pos.y + 1]);
-				b2 = true;
-			}
-		}
+	private List<Position> getCompletedPositions(Position pos, List<Direction> directions) {
+		Color color = squares[pos.x][pos.y].getBall().getColor();
+		var completedSquares = directions.stream().map(d -> new Position(pos.x + d.getX(), pos.y + d.getY()))
+				.filter(SquareStrategy.this::isValidPosition).filter(p -> squares[p.x][p.y].isEnableDestroy(color))
+				.toList();
+		return completedSquares.size() == 3 ? completedSquares : List.of();
+	}
 
-		if (pos.x < col - 1 && pos.y < row - 1) {
-			if (squareArray[pos.x][pos.y + 1].isEnableDestroy(color)
-					&& squareArray[pos.x + 1][pos.y + 1].isEnableDestroy(color)
-					&& squareArray[pos.x + 1][pos.y].isEnableDestroy(color)
-
-			) {
-				if (!b2) {
-					listCompleteSquare.add(squareArray[pos.x][pos.y + 1]);
-				}
-				listCompleteSquare.add(squareArray[pos.x + 1][pos.y + 1]);
-				listCompleteSquare.add(squareArray[pos.x + 1][pos.y]);
-				b3 = true;
-			}
-		}
-
-		if (pos.x < col - 1 && pos.y > 0) {
-			if (squareArray[pos.x + 1][pos.y].isEnableDestroy(color)
-					&& squareArray[pos.x + 1][pos.y - 1].isEnableDestroy(color)
-					&& squareArray[pos.x][pos.y - 1].isEnableDestroy(color)
-
-			) {
-				if (!b3) {
-					listCompleteSquare.add(squareArray[pos.x + 1][pos.y]);
-				}
-				listCompleteSquare.add(squareArray[pos.x + 1][pos.y - 1]);
-
-				if (!b1) {
-					listCompleteSquare.add(squareArray[pos.x][pos.y - 1]);
-				}
-			}
-		}
-
-		if (listCompleteSquare.size() > 0) {
-			listCompleteSquare.add(squareArray[pos.x][pos.y]);
-		}
-
-		return listCompleteSquare;
+	private boolean isValidPosition(Position pos) {
+		return pos.x >= 0 && pos.x < getColCount() && pos.y >= 0 && pos.y < getRowCount();
 	}
 }
