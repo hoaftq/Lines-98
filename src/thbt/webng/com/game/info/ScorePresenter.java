e2776fc233430ba2e2a17b2906caa4b4e7e698c2
hoaftq <hoaftq@gmail.com>
2023-02-25 00:14:36 +0800

    Refactored score using MVP

---
+++ src/thbt/webng/com/game/info/ScorePresenter.java
@@ -0,0 +1,44 @@
+package thbt.webng.com.game.info;
+
+import java.awt.*;
+
+public class ScorePresenter {
+
+    private ScoreModel model;
+
+    private ScoreView view;
+
+    public ScorePresenter(ScoreModel model, ScoreView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void draw(Graphics g) {
+        view.draw(g);
+    }
+
+    public void setLeft(int left) {
+        view.setLeft(left);
+    }
+
+    public void setTop(int top) {
+        view.setTop(top);
+    }
+
+    public int getScore() {
+        return model.getScore();
+    }
+
+    public void setScore(int score) {
+        model.setScore(score);
+        view.setDigitValues(model.getDigitValues());
+    }
+
+    public int getWidth() {
+        return view.getWidth();
+    }
+
+    public int getHeight() {
+        return view.getHeight();
+    }
+}
