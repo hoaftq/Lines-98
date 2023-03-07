a84269e0ca30b543ef2fccd912cb87a292b256df
hoaftq <hoaftq@gmail.com>
2023-03-07 11:15:29 +0800

    Refactored about dialog

--- src/thbt/webng/com/AboutDialog.java
+++ src/thbt/webng/com/AboutDialog.java
@@ -1,38 +1,35 @@
 package thbt.webng.com;
 
-import thbt.webng.com.game.util.WindowUtil;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.Desktop.Action;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.io.Serial;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-/**
- * Puzzle about dialog
- */
 public class AboutDialog extends JDialog {
-
-    private static final String VERSION = "1.0.0";
-    /**
-     * My home page address
-     */
-    private static final String THBT_WEB_ADDRESS = "https://github.com/hoaftq/Lines-98";
+    @Serial
     private static final long serialVersionUID = 1L;
-    /**
-     * Create about dialog
-     *
-     * @param owner owner of the dialog
-     */
+    private static final String VERSION = "1.0.0";
+    private static final String THBT_WEB_ADDRESS = "https://github.com/hoaftq/Lines-98";
+
     public AboutDialog(JFrame owner) {
         super(owner, "About", true);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-        // Create puzzle information label
-        JLabel infoLabel = new JLabel("""
+        add(createInfoPanel(), BorderLayout.NORTH);
+        add(createLinkPanel(), BorderLayout.CENTER);
+        add(createActionPanel(), BorderLayout.SOUTH);
+
+        pack();
+        setLocationRelativeTo(this);
+        setResizable(false);
+    }
+
+    private JPanel createInfoPanel() {
+        var infoLabel = new JLabel("""
                 <html>
                 	<table>
                 		<tr>
@@ -49,66 +46,62 @@ public class AboutDialog extends JDialog {
                 	</table>
                 </html>
                 """.formatted(VERSION));
-        JPanel infoPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        var infoPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         infoPanel.add(infoLabel);
-        add(infoPanel, BorderLayout.NORTH);
+        return infoPanel;
+    }
 
-        final String enteredVisitLabel = "<html><a href='" + THBT_WEB_ADDRESS + "'><i>" + THBT_WEB_ADDRESS
-                + "</i></a></html>";
-        final String exitedVisitLabel = "<html><a href='" + THBT_WEB_ADDRESS + "'>" + THBT_WEB_ADDRESS + "</a></html>";
+    private JPanel createLinkPanel() {
+        var normalLinkLabel = "<html><a href='%s'>%s</a></html>".formatted(THBT_WEB_ADDRESS, THBT_WEB_ADDRESS);
 
-        // Create my home page panel
-        JPanel visitPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        visitPanel.add(new JLabel("Source:"));
+        var linkPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        linkPanel.add(new JLabel("Source:"));
 
-        JLabel visitLabel = new JLabel(exitedVisitLabel);
-        visitLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        var linkLabel = new JLabel(normalLinkLabel);
+        linkLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
-        // Make entered/exited animate and process my home page link clicked
-        visitLabel.addMouseListener(new MouseAdapter() {
+        // Make entered/exited animate and browse the link when clicked
+        linkLabel.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-
-                // Open my home page with default browser
-                Desktop desktop = Desktop.getDesktop();
-                if (desktop.isSupported(Action.BROWSE)) {
-                    try {
-                        desktop.browse(new URI(THBT_WEB_ADDRESS));
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    } catch (URISyntaxException ex) {
-                        ex.printStackTrace();
-                    }
-                }
+                openHomePage();
             }
 
             @Override
             public void mouseEntered(MouseEvent e) {
-                ((JLabel) e.getSource()).setText(enteredVisitLabel);
+                ((JLabel) e.getSource()).setText(
+                        "<html><a href='%s'><i>%s</i></a></html>".formatted(THBT_WEB_ADDRESS, THBT_WEB_ADDRESS));
             }
 
             @Override
             public void mouseExited(MouseEvent e) {
-                ((JLabel) e.getSource()).setText(exitedVisitLabel);
+                ((JLabel) e.getSource()).setText(normalLinkLabel);
+            }
+
+            private void openHomePage() {
+                if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                    try {
+                        Desktop.getDesktop().browse(new URI(THBT_WEB_ADDRESS));
+                    } catch (IOException | URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
-        visitPanel.add(visitLabel);
-        add(visitPanel, BorderLayout.CENTER);
 
-        // Create panel container OK button
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 15));
-        JButton okButton = new JButton("OK");
+        linkPanel.add(linkLabel);
+        return linkPanel;
+    }
+
+
+    private JPanel createActionPanel() {
+        var actionPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 15));
+        var okButton = new JButton("OK");
         okButton.addActionListener((e) -> {
-
-            // Destroy about dialog
             setVisible(false);
             dispose();
         });
-        buttonPanel.add(okButton);
-        add(buttonPanel, BorderLayout.SOUTH);
-
-        pack();
-        WindowUtil.centerOwner(this);
-        setResizable(false);
+        actionPanel.add(okButton);
+        return actionPanel;
     }
 }
